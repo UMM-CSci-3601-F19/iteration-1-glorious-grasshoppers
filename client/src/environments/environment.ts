0fff308de1b0fe30c9128b33d028895e8b0d06a1
Robert-Beane <beane031@morris.umn.edu>
2019-10-09 17:15:25 -0500

    Add apis for backendless

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,5 +6,9 @@
 export const environment = {
   envName: 'dev',
   production: false,
-  API_URL: 'http://localhost:4567/api/'
+  API_URL: 'http://localhost:4567/api/',
+  backendless: {
+    APP_ID: 'D5959C41-1602-21D6-FFF0-AC7FF58B9F00',
+    API_KEY: 'B1D6F4C8-534A-7536-FF4F-9C5A44A36800'
+  }
 };
