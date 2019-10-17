165dffbc7294c338a17c8ae51bd286f11e09b988
Robert-Beane <beane031@morris.umn.edu>
2019-10-16 20:21:27 -0500

    delete test for machine-list

--- client/src/app/machines/machine-list.component.spec.ts
+++ client/src/app/machines/machine-list.component.spec.ts
@@ -18,8 +18,5 @@ describe('MachineListComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  //
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+
 });
